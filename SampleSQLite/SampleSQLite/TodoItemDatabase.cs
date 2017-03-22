88ccfdb64fcfac156fa43af41c97c0591a3bac6a
erick <erick@DESKTOP-JIKBK0G>
2017-03-22 17:13:29 +0700

    Add project files.

---
+++ SampleSQLite/SampleSQLite/TodoItemDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+
+namespace SampleSQLite
+{
+    
+    public class TodoItemDatabase
+    {
+        SQLiteConnection database;
+
+        public TodoItemDatabase()
+        {
+            database = DependencyService.Get<>();
+        }
+    }
+}
