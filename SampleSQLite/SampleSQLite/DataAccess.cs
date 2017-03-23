688e99ca36b69c9d6c538b62f98ed1c0d396baf6
erick <erick@DESKTOP-JIKBK0G>
2017-03-23 10:36:30 +0700

    menambahkan method pada Data Access

--- SampleSQLite/SampleSQLite/DataAccess.cs
+++ SampleSQLite/SampleSQLite/DataAccess.cs
@@ -4,11 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite.Net;
+using Xamarin.Forms;
 
 namespace SampleSQLite
 {
     public class DataAccess
     {
+        SQLiteConnection dbConn;
 
+        public DataAccess()
+        {
+            dbConn = DependencyService.Get<ISQLite>().GetConnection();
+            dbConn.CreateTable<Employee>();
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            return dbConn.Query<Employee>("select * from Employee order by EmpName asc");
+        }
+
+        public int InsertEmployee(Employee employee)
+        {
+            try
+            {
+                return dbConn.Insert(employee);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error : " + ex.Message);
+            }
+        }
+
+        public int DeleteEmployee(Employee employee)
+        {
+            try
+            {
+                return dbConn.Delete(employee);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error : " + ex.Message);
+            }
+        }
+
+        public int UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                return dbConn.Update(employee);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error : "+ex.Message);
+            }
+        } 
     }
 }
